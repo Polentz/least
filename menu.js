3edfd6c0f649f72e9ed9c54bdb2f7f1f38b89326
Giulia Polenta <giuliapolenta@gmail.com>
2022-03-25 16:34:28 +0100

    switch to a different layout

--- menu.js
+++ menu.js
@@ -1,45 +1,92 @@
-const openMenu = document.querySelector(".open-menu");
-const openBtn = document.querySelector(".open-btn");
-const closeMenu = document.querySelector(".close-menu");
-const closeBtn = document.querySelector(".close-btn");
+const openMenu = document.querySelector(".menu-btn");
+const closeMenu = document.querySelector(".close-btn");
 const menu = document.querySelector(".menu");
-const menuLabel = document.querySelector(".nav-label");
-const menuLinks = document.querySelectorAll(".menu a");
+const menuLabel = document.querySelector(".menu-trigger");
+const langBtn = document.querySelector(".lang a");
+const body = document.querySelector("body");
+const linkMenu = document.querySelectorAll(".menu-link a");
 
 openMenu.addEventListener("click", () => {
-    openBtn.classList.add("scale");
+    openMenu.classList.add("scale");
+    if (openMenu.classList.contains("scale")) {
+        closeMenu.classList.add("visible");
+    } else {
+        closeMenu.classList.remove("visible");
+    }
     setTimeout(() => {
         menu.classList.add("open");
+        menuLabel.classList.add("change");
+        menuLabel.innerHTML = "Close";
+        langBtn.style.color = "var(--bg-color)";
     }, 500);
+    setTimeout(() => {
+        body.style.overflowY = "hidden";
+    }, 600);
 });
 
 closeMenu.addEventListener("click", () => {
+    openMenu.classList.remove("scale");
     menu.classList.remove("open");
+    closeMenu.classList.remove("visible");
     setTimeout(() => {
-        openBtn.classList.remove("scale");
-    }, 500);
+        body.style.overflowY = "scroll";
+    }, 200)
+    setTimeout(() => {
+        menuLabel.classList.remove("change");
+        menuLabel.innerHTML = "Menu";
+        langBtn.style.color = "var(--txt-color)";
+    }, 800);
 });
 
-menuLinks.forEach(link => {
+linkMenu.forEach(link => {
     link.addEventListener("click", (event) => {
         event.preventDefault();
+        openMenu.classList.remove("scale");
         menu.classList.remove("open");
+        closeMenu.classList.remove("visible");
         setTimeout(() => {
-            openBtn.classList.remove("scale");
-        }, 500);
+            body.style.overflowY = "scroll";
+        }, 200);
         const url = link.getAttribute("href");
         setTimeout(() => {
+            menuLabel.classList.remove("change");
+            menuLabel.innerHTML = "Menu";
+            langBtn.style.color = "var(--txt-color)";
             document.location.href = url;
-        }, 1300);
+        }, 800);
     });
 });
 
+let lastAssignedColor;
+linkMenu.forEach(link => {
+    link.addEventListener("mouseenter", () => {
+        let hoverColors = ["var(--green)", "var(--purple)", "var(--blue)", "var(--yellow)"];
+        let randomIndex = Math.floor(Math.random() * hoverColors.length);
+        let randomColor = hoverColors[randomIndex];
+        if (randomColor === lastAssignedColor) {
+            const nextIndex = randomIndex + 1 > hoverColors.length - 1 ? 0 : randomIndex + 1;
+            randomColor = hoverColors[nextIndex];
+        }
+        lastAssignedColor = randomColor;
+        link.style.color = randomColor;
+    });
+    link.addEventListener("mouseleave", () => {
+        link.style.color = "var(--bg-color)";
+    });
+});
 
-
-
-
-
-
-
+const topBar = document.querySelector(".header");
+let lastScrollTop = 0;
+window.addEventListener("scroll", () => {
+    let scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+    if (scrollTop < lastScrollTop) {
+        topBar.classList.remove("up");
+        topBar.classList.add("down");
+    } else {
+        topBar.classList.remove("down");
+        topBar.classList.add("up");
+    }
+    lastScrollTop = scrollTop;
+}, false);
 
 
